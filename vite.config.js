fdd4544782235479a7e23d2671079151e4ffa600
dengfengqi <992921910@qq.com>
2025-10-22 10:13:55 +0800

    更新本地修改

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
+  base: './',             // 设置相对路径，这样打包后的 HTML 引用 JS/CSS 都会用相对路径
   build: {
-    outDir: 'dist',
+    outDir: 'docs',       // 打包输出到 docs 目录
+    emptyOutDir: true,    // 每次打包前清空 docs
     rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'),
       output: {
         // 打包成单个 IIFE 或 UMD 文件
         format: 'iife',
